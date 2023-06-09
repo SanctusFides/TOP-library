3978d01a493e2075f27f26d9d22e807793d278cf
John <jhn.hines@gmail.com>
2023-06-09 16:29:56 -0500

    Table added to page and logic created to update and avoid duped info

--- script.js
+++ script.js
@@ -19,29 +19,9 @@ window.onclick = function (event) {
 }
 // EVERYTHING BELOW IS NON-MODAL BOILER CODE
 
-
-var save = document.getElementById('saveBtn');
-// function for when save button is clicked - reads the values from form, packages them into book form and adds to library list
-save.onclick = function (event) {
-  event.preventDefault();
-  modal.style.display = "none";
-
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
-  const status = document.getElementById('readStatus').checked;
-
-  const book = new Book(title, author, pages, status);
-  // console.log(book);
-  myLibrary.push(book);
-
-  const form = document.getElementById('bookForm')
-  form.reset();
-}
-
-
-const table = document.querySelector('#table');
-
+// list that store the books to create a table from
+let myLibrary = [];
+// the Book constructor
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
@@ -52,16 +32,64 @@ function Book(title, author, pages, read) {
   }
 }
 
-let myLibrary = [];
 
-function TableUpdate(book) {
+// function for when save button is clicked
+var save = document.getElementById('saveBtn');
+save.onclick = function (event) {
+  // Prevents page error and clears the modal from the screen
+  event.preventDefault();
+  modal.style.display = "none";
 
+  // Collects the user fields and packages them as a book- adding it to the list
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const status = document.getElementById('readStatus').checked;
+
+  const book = new Book(title, author, pages, status);
+  myLibrary.push(book);
+  tableUpdate();
+  // Resets the form fields for the next entry
+  const form = document.getElementById('bookForm')
+  form.reset();
 }
 
 
 
-// Used for test printing to log
-function Print() {
+function tableUpdate() {
+
+  let table = document.getElementById('Library');
+
+  // Each time the update is run, these the block below gathers all the books and clears them from the table so that
+  // it can repopulate the table and avoid duplicating the table data when a new book is added
+  let rows = document.querySelectorAll('#Book');
+  rows.forEach(row => {
+    row.remove();
+  })
+
+  // Simply loops through the book list and packages together a row to be inserted into the table
+  myLibrary.forEach(book => {
+    const title = book.title;
+    const author = book.author;
+    const pages = book.pages;
+    const read = book.read;
+    let row = table.insertRow();
+    row.setAttribute("id", "Book");
+    let titleCell = row.insertCell();
+    titleCell.textContent = title;
+    let authorCell = row.insertCell();
+    authorCell.textContent = author;
+    let pagesCell = row.insertCell();
+    pagesCell.textContent = pages;
+    // console.log(`${title} ${author} ${pages} ${read}`);
+  });
+  // console.log(rows);
+}
+
+
+
+// Used for test printing to console
+function print() {
   myLibrary.forEach(element => {
     console.log(element);
   });
